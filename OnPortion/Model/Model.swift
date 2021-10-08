92e052ad862d73da32d6f8e5a56b85fe6274fa36
DaneOnishi <daniellaonishi07@gmail.com>
2021-10-08 01:13:43 -0300

    Starts Model and adds screens initial layouts

--- OnPortion/Model/Model.swift
+++ OnPortion/Model/Model.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class ModelSingleton {
+    static let shared = ModelSingleton()
+    private init() {}
+    let glassFlour = 200
+    let oneSoupSpoonFlour = 7.5
+    let oneTeaSpoonFlour = 2.5
+    let glassCocoa = 180
+    let oneSoupSpoonCocoa = 6
+    let oneTeaSpoonCocoa = 2.5
+    let glassSugar = 360
+    let cupSugar = 180
+    let oneSoupSpoonSugar = 6
+    let oneTeaSpoonSugar = 2.5
+    
+    
+
+    func measureFlour() {
+    
+    }
+    
+    enum measurementRule {
+        case flour
+        case cocoa
+        case sugar
+        case cornStarch
+        case milk
+ 
+            }
+        }
